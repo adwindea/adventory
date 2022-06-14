e7f4d86ae69ce42727c3ca075637fd872f5c4056
Dan Adwindea <danadwnd@gmail.com>
2022-06-14 09:04:54 +0000

    Tidy up table

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,12 +11,13 @@ class ProductController extends Controller
     function index(Request $request){
         (empty($request->sort)) ? $request->sort = 'name' : '';
         (empty($request->direction)) ? $request->direction = 'asc' : '';
+        (empty($request->perPage)) ? $request->perPage = 10 : '';
         $products = Product::orderBy($request->sort, $request->direction)
         ->when(!empty($request->search), function ($query) use ($request){
             return $query->where('name', 'LIKE', '%'.$request->search.'%')
                 ->orWhere('sku', 'LIKE', '%'.$request->search.'%');
         })
-        ->paginate(2)
+        ->paginate($request->perPage)
         ->onEachSide(-1)
         ->withQueryString()
         ->through(fn ($product) => [
@@ -28,7 +29,10 @@ class ProductController extends Controller
         ]);
         return Inertia::render('Product/Products', [
             'filters' => [
-                'search' => $request->form
+                'search' => $request->form,
+                'perPage' => $request->perPage,
+                'sort' => $request->sort,
+                'direction' => $request->direction
             ],
             'products' => $products
         ]);
--- resources/js/Pages/Product/Products.vue
+++ resources/js/Pages/Product/Products.vue
@@ -10,63 +10,81 @@
 
         <div class="card w-full bg-white dark:bg-dark-eval-1 shadow-xl">
             <div class="card-body">
-                <div class="card-title justify-start">
-                    <input type="text" placeholder="Type to search" v-model="form.search" class="input input-bordered w-full max-w-xs" />
-                    <!-- <div class="dropdown dropdown-right">
-                        <label tabindex="0" class="btn m-1">Filter</label>
-                        <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-                            <li><a>Item 1</a></li>
-                            <li><a>Item 2</a></li>
-                        </ul>
-                    </div> -->
-                    <button class="btn px-2" @click="reset">Reset</button>
+                <div class="card-title flex-wrap justify-between">
+                    <div class="flex-wrap w-full max-w-lg">
+                        <input type="text" placeholder="Type to search" v-model="form.search" class="input input-bordered w-full max-w-xs mr-2 mb-2" />
+                        <div class="dropdown dropdown-right">
+                            <label tabindex="0" class="btn mr-2">Filter</label>
+                            <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                                <li><a>Item 1</a></li>
+                                <li><a>Item 2</a></li>
+                            </ul>
+                        </div>
+                        <button class="btn" @click="reset">Reset</button>
+                    </div>
+                    <div class="flex w-full max-w-xs items-center md:justify-end justify-between">
+                        <div class="w-3/8">
+                            <label class="label block">
+                                <span class="label-text">Result per Page</span>
+                            </label>
+                        </div>
+                        <div class="w-5/8">
+                            <select class="select select-bordered w-full max-w-xs" v-model="form.perPage">
+                                <option v-for="option in perPageOption" :key="option" :value="option">
+                                    {{option}}
+                                </option>
+                            </select>
+                        </div>
+                    </div>
                 </div>
-
                 <div class="overflow-x-auto">
                     <table class="table w-full">
                         <thead>
                             <tr>
+                                <th class="text-center">No</th>
                                 <th class="text-center" @click="setOrderBy('sku')">SKU
                                     <span class="float-right">
-                                        <faicon icon="arrow-up" v-if="sort=='sku' && direction=='asc'"/>
-                                        <faicon icon="arrow-down" v-if="sort=='sku' && direction=='desc'"/>
+                                        <faicon icon="arrow-up" v-if="form.sort=='sku' && form.direction=='asc'"/>
+                                        <faicon icon="arrow-down" v-if="form.sort=='sku' && form.direction=='desc'"/>
                                     </span>
                                 </th>
                                 <th @click="setOrderBy('name')">Name
                                     <span class="float-right">
-                                        <faicon icon="arrow-up" v-if="sort=='name' && direction=='asc'"/>
-                                        <faicon icon="arrow-down" v-if="sort=='name' && direction=='desc'"/>
+                                        <faicon icon="arrow-up" v-if="form.sort=='name' && form.direction=='asc'"/>
+                                        <faicon icon="arrow-down" v-if="form.sort=='name' && form.direction=='desc'"/>
                                     </span>
                                 </th>
                                 <th class="text-center" @click="setOrderBy('quantity')">Quantity
                                     <span class="float-right">
-                                        <faicon icon="arrow-up" v-if="sort=='quantity' && direction=='asc'"/>
-                                        <faicon icon="arrow-down" v-if="sort=='quantity' && direction=='desc'"/>
+                                        <faicon icon="arrow-up" v-if="form.sort=='quantity' && form.direction=='asc'"/>
+                                        <faicon icon="arrow-down" v-if="form.sort=='quantity' && form.direction=='desc'"/>
                                     </span>
                                 </th>
                                 <th class="text-center" @click="setOrderBy('price')">Price
                                     <span class="float-right">
-                                        <faicon icon="arrow-up" v-if="sort=='price' && direction=='asc'"/>
-                                        <faicon icon="arrow-down" v-if="sort=='price' && direction=='desc'"/>
+                                        <faicon icon="arrow-up" v-if="form.sort=='price' && form.direction=='asc'"/>
+                                        <faicon icon="arrow-down" v-if="form.sort=='price' && form.direction=='desc'"/>
                                     </span>
                                 </th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="product in products.data" :key="product.xid">
+                            <tr v-for="(product, index) in products.data" :key="product.xid">
+                                <td class="text-center">{{index+1}}</td>
                                 <td class="text-center">{{product.sku}}</td>
                                 <td>{{product.name}}</td>
                                 <td class="text-center">{{product.quantity}}</td>
                                 <td class="text-center">{{product.price}}</td>
                             </tr>
                             <tr v-if="products.data.length === 0">
-                                <td class="text-center" colspan="4">No products found.</td>
+                                <td class="text-center" colspan="5">No products found.</td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
 
-                <div class="card-actions justify-between">
+                <div class="card-actions items-center justify-between">
+                    <span>Found {{products.total}} data.</span>
                     <div class="btn-group">
                         <Link v-for="paging in products.links" :href="paging.url" :key="paging.label"
                             class="btn"
@@ -98,11 +116,12 @@ export default {
     },
     data() {
         return {
-            direction: 'asc',
-            sort: 'name',
+            perPageOption: [10,25,50,100],
             form: {
                 search: this.filters.search,
-            //     trashed: this.filters.trashed,
+                direction: this.filters.direction,
+                sort: this.filters.sort,
+                perPage: this.filters.perPage,
             },
         }
     },
@@ -116,18 +135,18 @@ export default {
     },
     methods: {
         setOrderBy(orderBy) {
-            this.direction = this.direction == 'asc' ? 'desc' : 'asc'
-            this.sort = orderBy
-            this.$inertia.get(route('product'), {
-                'sort': orderBy,
-                'direction': this.direction
-            }, {
-                preserveState: true
-            })
-
+            this.form.direction = this.form.direction == 'asc' ? 'desc' : 'asc'
+            this.form.sort = orderBy
+            // this.$inertia.get(route('product'), {
+            //     'sort': orderBy,
+            //     'direction': this.direction
+            // }, {
+            //     preserveState: true
+            // })
         },
         reset() {
             this.form = mapValues(this.form, () => null)
+            this.form.perPage = this.filters.perPage
         },
     },
 }
